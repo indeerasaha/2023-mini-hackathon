19e5b844dd7794c27df4f60e7beaacf508158476
Aileen <you@example.com>
2023-12-03 13:16:02 -0500

    test

--- init.py
+++ init.py
@@ -17,3 +17,34 @@ conn = pymysql.connect(host='localhost',
 @app.route('/')
 def hello():
 	return render_template('index.html')
+
+@app.route('/Dashboard', methods=['GET', 'POST'])
+def Dashboard():
+	return render_template('Dashboard.html')
+
+#Authenticates the register
+@app.route('/registerAuth', methods=['GET', 'POST'])
+def registerAuth():
+	#grabs information from the forms
+	username = request.form['username']
+	password = request.form['password']
+
+	#cursor used to send queries
+	cursor = conn.cursor()
+	#executes query
+	query = 'SELECT * FROM user WHERE username = %s'
+	cursor.execute(query, (username))
+	#stores the results in a variable
+	data = cursor.fetchone()
+	#use fetchall() if you are expecting more than 1 data row
+	error = None
+	if(data):
+		#If the previous query returns data, then user exists
+		error = "This user already exists"
+		return render_template('register.html', error = error)
+	else:
+		ins = 'INSERT INTO user VALUES(%s, %s)'
+		cursor.execute(ins, (username, password))
+		conn.commit()
+		cursor.close()
+		return render_template('index.html')
